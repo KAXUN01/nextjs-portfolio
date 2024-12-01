79dab14d57eda9f5ab6785c47b6327f626e9c54b
Kasun madhushan wickramasinghe <119012876+KAXUN01@users.noreply.github.com>
2024-12-01 09:11:43 +0000

    add cv

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -46,7 +46,8 @@ const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           className="z-30 hover:scale-125 py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] relative transition-transform duration-300 ease-in-out"
-          href="#"
+          href="/resume.pdf"
+          target="_blank"
         >
           DOWNLOAD CV
         </motion.a>
